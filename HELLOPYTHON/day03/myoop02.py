2664b163eb19b32e9dd2461e6f8c355efa9005d3
eubnaeuu <cdwcdw34@gmail.com>
2021-05-04 21:50:12 +0900

    2021-05-04 21:50:11.38

--- HELLOPYTHON/day03/myoop02.py
+++ HELLOPYTHON/day03/myoop02.py
@@ -5,18 +5,23 @@ class JindoDog:
 
     def tranning(self):
         self.power_bark += 1
+
         
 class SokchoBird:
+
     def __init__(self):
         self.flag_Icanfly = False
+
     def practice(self):
         self.flag_Icanfly = True
 
-class Gaesae(JindoDog,SokchoBird):
-    def __init__(self):
-        #super(Human, self).__init__()    #super는 첫번째 부모만 인식하므로 다중상속하는 경우 해당 조상클래스를 직접 입력하며 self도 써줘야함
+
+class Gaesae(JindoDog, SokchoBird):
+
+    def __init__(self): # super(Human, self).__init__()    #super는 첫번째 부모만 인식하므로 다중상속하는 경우 해당 조상클래스를 직접 입력하며 self도 써줘야함
         JindoDog.__init__(self) 
         SokchoBird.__init__(self) 
+
 
 gaesae = Gaesae()
 print(gaesae.flag_Icanfly)
