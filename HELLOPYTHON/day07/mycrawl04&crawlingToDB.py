f0a65c9cd5122b6e7dc4dfcb6ea6cd068475173c
eubnaeuu <cdwcdw34@gmail.com>
2021-05-11 21:28:36 +0900

    2021-05-11 21:28:34.81

--- HELLOPYTHON/day07/mycrawl04&crawlingToDB.py
+++ HELLOPYTHON/day07/mycrawl04&crawlingToDB.py
@@ -6,8 +6,13 @@ import urllib.request
 from bs4 import BeautifulSoup
 import pymysql
 from bokeh.sphinxext.templates import BJS_CODEPEN_INIT
+import datetime
+
+
 
 def insertChicken(tuts):
+    
+    
     conn = pymysql.connect(
         user='root', 
         passwd='java', 
@@ -16,19 +21,15 @@ def insertChicken(tuts):
         charset='utf8'
     )
     curs = conn.cursor()
-    sql = "INSERT INTO stock(s_code, s_name, s_price, crawl_date) VALUES(%s,%s,%s,DATE_FORMAT(now(), '%y%m%d %h:%m'));"
+    sql = "INSERT INTO stock(s_code, s_name, s_price, crawl_date) VALUES(%s,%s,%s,%s);"
     cnt = curs.executemany(sql,tuts)
     conn.commit()
     conn.close()    
     return cnt
 
-client_id = "t6LiD84QhVfZT1L1rFXg"
-client_secret = "LH_y_XvemI"
 # encText = urllib.parse.quote("치킨")
 url = "https://vip.mk.co.kr/newSt/rate/item_all.php"
 request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
 response = urllib.request.urlopen(request)
 rescode = response.getcode()
 
@@ -47,37 +48,38 @@ if(rescode==200):
     #     s_tmp = item
     #     print(item)
     
-    for idx, item in enumerate(itemsTitle):
+    for i, item in enumerate(itemsTitle):
         s_name = item.get_text()
         s_code = item.attrs['title']
         listName.append(s_name) 
         listCode.append(s_code)
-        # print("item : ",item)
-        # print("name : ",s_name)
-        # print("code : ",s_code)
-        # print(item.title.get_Text())
+        
     for idx, item in enumerate(itemsPrice):
         s_price = item.get_text()
         listPrice.append(s_price) 
-        # print("price : ",s_price)
-        # title       = item.title.getText()
-        # tuts.append((title,description,link,bloggername,bloggerlink,postdate))
-    listans = []; 
-#     >> while treeHit < 10:
-# ...     treeHit = treeHit +1
-# ...     print("나무를 %d번 찍었습니다." % treeHit)
-# ...     if treeHit == 10:
-
+        
+    listans = [];
+     
+    now = datetime.datetime.now()
+    formattedDate = now.strftime("%y%m%d_%H%M")
+    print(formattedDate)
+    
     for i in range(0,len(listCode)):
         a_name =  listName[i]
         a_code =  listCode[i]
         a_price =  listPrice[i]
-        while((listPrice[i].find(',')) != -1):
-            idx = listPrice[i].find(',')
-            a_price = int(a_price[0:idx]+a_price[idx+1:])
-            print(a_price)
-            listans.append((a_name,a_code,a_price))
+        crawl_date = formattedDate 
         
-    # cnt = insertChicken(listans)
+        while((a_price.find(',')) != -1):
+            idx = a_price.find(',')
+            a_price = a_price[0:idx]+a_price[idx+1:]
+            
+        listans.append((a_code,a_name,int(a_price),crawl_date))
+        
+    print(listans)
+    
+    cnt = insertChicken(listans)
+    print(cnt)
+    
 else:
     print("Error Code:" + rescode)
