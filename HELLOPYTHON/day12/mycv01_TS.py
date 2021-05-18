f568727f358759b93d5a76d48f8f76ef53473be7
eubnaeuu <cdwcdw34@gmail.com>
2021-05-18 21:50:16 +0900

    2021-05-18 21:50:09.53

--- HELLOPYTHON/day12/mycv01_TS.py
+++ HELLOPYTHON/day12/mycv01_TS.py
@@ -4,13 +4,12 @@ from cv2.cv2 import cvtColor
 # 이미지 읽기
 img = cv2.imread('test.PNG', 1) # 3x2 픽셀
 
-img_color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_rb = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_rb = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # to 흑백
  
 print(img_rb)
  
 # 이미지 화면에 표시
-cv2.imshow('Test Image1', img_color)
+cv2.imshow('Test Image1', img)
 cv2.imshow('Test Image2', img_rb)
 cv2.waitKey(0)
 
